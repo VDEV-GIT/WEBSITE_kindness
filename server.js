8c0bbe938e4c61676c3e7fac4f1687de3e43e02d
Vince <49102355+VDEV-GIT@users.noreply.github.com>
2025-06-22 16:11:08 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
+const app = express();
+const db = new sqlite3.Database('./notes.db');
+const cors = require('cors');
+
+
+app.use(cors());
+
+app.use(express.json());
+
+app.post('/notes', (req, res) => {
+  const { content } = req.body;
+  db.run('INSERT INTO notes (content) VALUES (?)', [content], function(err) {
+    if (err) return res.status(500).send('Error saving note');
+    res.send({ id: this.lastID });
+  });
+});
+
+app.get('/notes', (req, res) => {
+  db.all('SELECT * FROM notes ORDER BY created_at DESC LIMIT 5', [], (err, rows) => {
+    if (err) return res.status(500).send('Error fetching notes');
+    res.json(rows);
+  });
+});
+
+app.listen(3000, () => console.log('Server running on http://localhost:3000'));
